f05b436294c6b6062932b4707b5a09e85986baac
Spinback <104303481+Spinbackk@users.noreply.github.com>
2022-04-24 15:03:33 +0000

    Gui Improvements

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace LCProxy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(12, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "What is Alpha Proxy?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(1089, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Indigo;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(793, 530);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(315, 83);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Install Alpha Proxy";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Indigo;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 530);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(315, 83);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Uninstall Alpha Proxy";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.label3.Font = new System.Drawing.Font("Bahnschrift", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(31, 305);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(454, 90);
+            this.label3.TabIndex = 6;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.label4.Font = new System.Drawing.Font("Bahnschrift", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Location = new System.Drawing.Point(9, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(264, 33);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Should you trust us?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.label5.Font = new System.Drawing.Font("Bahnschrift", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(31, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(406, 90);
+            this.label5.TabIndex = 8;
+            this.label5.Text = resources.GetString("label5.Text");
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Violet;
+            this.label6.Location = new System.Drawing.Point(84, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(193, 21);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Alpha Proxy | .gg/cosmetics";
+            this.label6.UseCompatibleTextRendering = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LCProxy.Properties.Resources.alpha_big;
+            this.pictureBox2.Location = new System.Drawing.Point(590, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(472, 448);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::LCProxy.Properties.Resources.alpha_rpoxy;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 64);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1120, 625);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Lunar Unlocker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LCProxy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        protected override void WndProc(ref Message message)
+        {
+            base.WndProc(ref message);
+
+            if (message.Msg == WM_NCHITTEST && (int)message.Result == HTCLIENT)
+                message.Result = (IntPtr)HTCAPTION;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.gg/cosmetics");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string contents = System.IO.File.ReadAllText("C:\\Windows\\System32\\drivers\\etc\\hosts");
+            if (contents.Contains("51.195.220.243 assetserver.lunarclientprod.com"))
+                contents = contents.Replace("51.195.220.243 assetserver.lunarclientprod.com", "");
+            if (contents.Contains("194.163.177.249 assetserver.lunarclientprod.com"))
+                contents = contents.Replace("194.163.177.249 assetserver.lunarclientprod.com", "");
+            System.IO.File.WriteAllText("C:\\Windows\\System32\\drivers\\etc\\hosts", contents);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Installing Alpha Proxy!";
+            installLCP();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileSecurity accessControl = System.IO.File.GetAccessControl("C:\\Windows\\System32\\drivers\\etc\\hosts");
+            accessControl.AddAccessRule(new FileSystemAccessRule((IdentityReference)new SecurityIdentifier(WellKnownSidType.WorldSid, (SecurityIdentifier)null), FileSystemRights.FullControl, AccessControlType.Allow));
+            System.IO.File.SetAccessControl("C:\\Windows\\System32\\drivers\\etc\\hosts", accessControl);
+            string contents = System.IO.File.ReadAllText("C:\\Windows\\System32\\drivers\\etc\\hosts");
+            if (contents.Contains("198.251.84.251 assetserver.lunarclientprod.com"))
+                contents = contents.Replace("198.251.84.251 assetserver.lunarclientprod.com", "");
+            System.IO.File.WriteAllText("C:\\Windows\\System32\\drivers\\etc\\hosts", contents);
+            accessControl.RemoveAccessRule(new FileSystemAccessRule((IdentityReference)new SecurityIdentifier(WellKnownSidType.WorldSid, (SecurityIdentifier)null), FileSystemRights.FullControl, AccessControlType.Allow));
+            System.IO.File.SetAccessControl("C:\\Windows\\System32\\drivers\\etc\\hosts", accessControl);
+            button2.Text = "Uninstalled Alpha Proxy!";
+        }
+        private void installLCP()
+        {
+            RegistryKey registryKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey("SOFTWARE\\JavaSoft\\Java Runtime Environment\\1.8");
+            if (registryKey != null)
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.DownloadFile("https://assets.lunarproxy.me/server.cer", Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LCPcert.cer"));
+                    string str1 = (string)registryKey.GetValue("JavaHome");
+                    string str2 = "zulu";
+                    foreach (FileSystemInfo fileSystemInfo in new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\.lunarclient\\jre").GetFileSystemInfos("*" + str2 + "*"))
+                    {
+                        string fullName = fileSystemInfo.FullName;
+                        Process.Start(str1 + "\\bin\\keytool.exe", "-keystore \"" + fullName + "\\lib\\security\\cacerts\" -trustcacerts -importcert -alias lcproxy -storepass changeit -file \"" + Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LCPcert.cer") + "\" -noprompt");
+                    }
+                    webClient.Dispose();
+                }
+                if (!System.IO.File.Exists("C:\\Windows\\System32\\drivers\\etc\\hosts"))
+                    System.IO.File.Create("C:\\Windows\\System32\\drivers\\etc\\hosts");
+                FileSecurity accessControl = System.IO.File.GetAccessControl("C:\\Windows\\System32\\drivers\\etc\\hosts");
+                accessControl.AddAccessRule(new FileSystemAccessRule((IdentityReference)new SecurityIdentifier(WellKnownSidType.WorldSid, (SecurityIdentifier)null), FileSystemRights.FullControl, AccessControlType.Allow));
+                System.IO.File.SetAccessControl("C:\\Windows\\System32\\drivers\\etc\\hosts", accessControl);
+                string contents = System.IO.File.ReadAllText("C:\\Windows\\System32\\drivers\\etc\\hosts");
+                if (!contents.Contains("198.251.84.251 assetserver.lunarclientprod.com"))
+                    contents += "\n198.251.84.251 assetserver.lunarclientprod.com";
+                System.IO.File.WriteAllText("C:\\Windows\\System32\\drivers\\etc\\hosts", contents);
+                accessControl.RemoveAccessRule(new FileSystemAccessRule((IdentityReference)new SecurityIdentifier(WellKnownSidType.WorldSid, (SecurityIdentifier)null), FileSystemRights.FullControl, AccessControlType.Allow));
+                System.IO.File.SetAccessControl("C:\\Windows\\System32\\drivers\\etc\\hosts", accessControl);
+                button1.Text = "Installed Alpha Proxy!";
+            }
+            else
+                new Thread(new ThreadStart(this.noJava)).Start();
+        }
+        private void noJava()
+        {
+            if (MessageBox.Show("You are missing JDK 1.8, would you like to install it?\nThis may take up to 5 minutes.", "Missing JDK", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    Process.Start("");
+                }
+            }
+            else
+            {
+                int num = (int)MessageBox.Show("The installation cannot continue.", "Missing JDK", MessageBoxButtons.OK);
+                Application.Exit();
+            }
+        }
+    }
+}
